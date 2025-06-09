f9ef42230a6cd199717aee9a4cbf0bfc7ff56a46
TheAnhDev17 <theanh.dev17@gmail.com>
2025-06-09 11:55:26 +0700

    Initial project

---
+++ src/main/java/com/example/ecommerce/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String id;
+
+    String username;
+    String password;
+    String firstName;
+    String lastName;
+    LocalDate dob;
+}
