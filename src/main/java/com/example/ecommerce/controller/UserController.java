bd0e93f1684eb846632eece4bec191eb8e5e267a
TheAnhDev17 <theanh.dev17@gmail.com>
2025-06-10 16:28:08 +0700

    User: create user

--- src/main/java/com/example/ecommerce/controller/UserController.java
+++ src/main/java/com/example/ecommerce/controller/UserController.java
@@ -1,7 +1,26 @@
 package com.example.ecommerce.controller;
 
-import org.springframework.stereotype.Controller;
+import com.example.ecommerce.dto.request.UserCreationRequest;
+import com.example.ecommerce.entity.User;
+import com.example.ecommerce.service.UserService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserController {
+
+    UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody UserCreationRequest request){
+        return userService.createUser(request);
+    }
 }
