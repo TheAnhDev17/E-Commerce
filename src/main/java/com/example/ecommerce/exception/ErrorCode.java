0d07f339e4879c3f0ebe83eb1c4af8f6931eac57
TheAnhDev17 <theanh.dev17@gmail.com>
2025-06-12 16:02:36 +0700

    Handling Exception and ApiResponse

--- src/main/java/com/example/ecommerce/exception/ErrorCode.java
+++ src/main/java/com/example/ecommerce/exception/ErrorCode.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatusCode;
 @Getter
 public enum ErrorCode {
 
-    USER_EXISTED(1000, "User existed", HttpStatus.BAD_REQUEST);
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_EXISTED(1002, "User existed", HttpStatus.BAD_REQUEST);
+
 
     private final int code;
     private final String message;
