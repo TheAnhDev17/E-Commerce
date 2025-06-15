47ed34df3bf2d47baa1a18806469217606546859
TheAnhDev17 <theanh.dev17@gmail.com>
2025-06-15 15:36:41 +0700

    CRUD user

--- src/main/java/com/example/ecommerce/service/UserService.java
+++ src/main/java/com/example/ecommerce/service/UserService.java
@@ -13,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -38,4 +40,17 @@ public class UserService {
         return userMapper.toUserResponse(userRepository.save(user));
     }
 
+    public UserResponse getUser(String userId){
+        return userRepository.findById(userId).map(userMapper::toUserResponse)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+    }
+
+    public List<UserResponse> getUsers(){
+        return userRepository.findAll().stream().map(userMapper::toUserResponse).toList();
+    }
+
+    public void deleteUser(String userId){
+
+        userRepository.deleteById(userId);
+    }
 }
