d2b41839332e30bca28f3859f693de7e4a32939f
TheAnhDev17 <theanh.dev17@gmail.com>
2025-06-16 15:41:47 +0700

    Feature: encoded and matches password

---
+++ src/main/java/com/example/ecommerce/service/AuthenticationService.java
@@ -0,0 +1,28 @@
+package com.example.ecommerce.service;
+
+import com.example.ecommerce.dto.request.AuthenticationRequest;
+import com.example.ecommerce.entity.User;
+import com.example.ecommerce.exception.AppException;
+import com.example.ecommerce.exception.ErrorCode;
+import com.example.ecommerce.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationService {
+    UserRepository userRepository;
+
+    public boolean authenticate(AuthenticationRequest request){
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        return passwordEncoder.matches(request.getPassword(), user.getPassword());
+    }
+}
--- src/main/java/com/example/ecommerce/service/UserService.java
+++ src/main/java/com/example/ecommerce/service/UserService.java
@@ -11,6 +11,8 @@ import com.example.ecommerce.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,7 +29,11 @@ public class UserService {
         if(userRepository.existsByUsername(request.getUsername()))
            throw new AppException(ErrorCode.USER_EXISTED);
 
-        return userMapper.toUserResponse(userRepository.save(userMapper.toUser(request)));
+        User user = userMapper.toUser(request);
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        return userMapper.toUserResponse(userRepository.save(user));
     }
 
     public UserResponse updateUser(String userId, UserUpdateRequest request) {
@@ -36,6 +42,8 @@ public class UserService {
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         userMapper.updateUser(user, request);
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
 
         return userMapper.toUserResponse(userRepository.save(user));
     }
