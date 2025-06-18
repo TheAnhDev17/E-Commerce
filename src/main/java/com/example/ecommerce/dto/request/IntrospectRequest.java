7c1205df35ff027dd051e4be18a5dfccf579ea82
TheAnhDev17 <theanh.dev17@gmail.com>
2025-06-18 09:16:00 +0700

    JWT

--- src/main/java/com/example/ecommerce/dto/request/IntrospectRequest.java
+++ src/main/java/com/example/ecommerce/dto/request/IntrospectRequest.java
@@ -1,4 +1,13 @@
 package com.example.ecommerce.dto.request;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class IntrospectRequest {
+    String token;
 }
